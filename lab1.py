c21d81d6fa39fe2331560fc2baa1b3366d97eadd
Senichkaa <super-kapanaiko406@ukr.net>
2023-09-14 17:41:29 +0300

    added memory writing a results

--- lab1.py
+++ lab1.py
@@ -1,49 +1,68 @@
 import math
 
+memory = []
 print("Starting...")
 
-value1 = float(input("Enter your first value: "))
-value2 = float(input("Enter your second value: "))
 
 while True:
-    print("1. +")
-    print("2. -")
-    print("3. *")
-    print("4. /")
-    print("5. ^")
-    print("6. √")
-    print("7. %")
-    chooseOperator = input("Enter a number of your arithmetic operator: ")
+    value1 = float(input("Enter your first value: "))
+    value2 = float(input("Enter your second value: "))
 
-    if chooseOperator not in ["+", "-", "*", "/", "^", "√", "%"]:
-        print(
-            "Invalid operator has been chosen. Please, follow the list and try one more time!. Please, reboot the program."
-        )
-        break
-    else:
-        if chooseOperator == "+":
-            print(value1 + value2)
-        elif chooseOperator == "-":
-            print(value1 - value2)
-        elif chooseOperator == "*":
-            print(value1 * value2)
-        elif chooseOperator == "/":
-            if value1 == 0 or value2 == 0:
-                print("You can not divide by zero")
-            else:
-                print(value1 / value2)
-        elif chooseOperator == "^":
-            print(value1**value2)
-        elif chooseOperator == "√":
-            print("Alert! the square root working only with first number!")
-            print(math.sqrt(value1))
-        elif chooseOperator == "%":
-            print((value1 / value2) * 100)
+    while True:
+        print("1. +")
+        print("2. -")
+        print("3. *")
+        print("4. /")
+        print("5. ^")
+        print("6. √")
+        print("7. %")
+        chooseOperator = input("Enter an arithmetic operator you prefer: ")
+
+        if chooseOperator not in ["+", "-", "*", "/", "^", "√", "%"]:
+            print(
+                "Invalid operator has been chosen. Please, follow the list and try one more time!. Please, reboot the program."
+            )
+            break
+        else:
+            if chooseOperator == "+":
+                result = value1 + value2
+                print(result)
+            elif chooseOperator == "-":
+                result = value1 - value2
+                print(result)
+            elif chooseOperator == "*":
+                result = value1 * value2
+                print(result)
+            elif chooseOperator == "/":
+                if value1 == 0 or value2 == 0:
+                    print("You can not divide by zero")
+                else:
+                    result = value1 / value2
+                    print(result)
+            elif chooseOperator == "^":
+                result = value1**value2
+                print(result)
+            elif chooseOperator == "√":
+                print("Alert! the square root working only with first number!")
+                result = math.sqrt(value1)
+                print(result)
+            elif chooseOperator == "%":
+                result = (value1 / value2) * 100
+                print(result)
+
+        saveIntoMemory = input("Do you want to save a result in memory? (Y/N): ")
+        if saveIntoMemory.lower() == "y":
+            memory.append(result)
+            print(result)
+            break
 
     moreCalc = input("Do you want make more calculations? (Y/N): ")
     if moreCalc.lower() != "y":
         print("Disposing...")
         break
-    else:
-        value1 = float(input("Enter your first value: "))
-        value2 = float(input("Enter your second value: "))
+    # else:
+    #     value1 = float(input("Enter your first value: "))
+    #     value2 = float(input("Enter your second value: "))
+
+if memory is not None:
+    print(f"Memory: {memory}")
