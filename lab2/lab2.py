f03dc60c2c015135fb16a2431f85c8c6b24b92d1
Senichkaa <super-kapanaiko406@ukr.net>
2023-10-03 14:37:47 +0300

    added func to choose operator

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -11,12 +11,34 @@ class Calculator:
     def input_numbers(self):
         self.value1 = float(input("Enter your first value: "))
         self.value2 = float(input("Enter your second value: "))
+        
+    # Вибір дії над числами 
+    def choose_operator(self):
+        while True:
+            print("1. +")
+            print("2. -")
+            print("3. *")
+            print("4. /")
+            print("5. ^")
+            print("6. √")
+            print("7. %")
+            self.choose_operator = input("Enter an arithmetic operator you prefer: ")
+
+            # Якщо некоректно - ерор в консоль + рестарт
+            if self.choose_operator not in ["+", "-", "*", "/", "^", "√", "%"]:
+                print(
+                    "Invalid operator has been chosen. Please, follow the list and try one more time!. Please, reboot the program."
+                )
+                break
+            else:
+                break
 
     # Старт програми, а саме виконання всіх блоків-функцій описаних вище
     def run(self):
         print("Starting...")
         while True:
             self.input_numbers()
+            self.choose_operator()
 
 # Основна частина програми
 if __name__ == "__main__":
